1a5f59e9abf403bf9b02d9b1808c09abe01ae264
AlexFonBlack <nevermor86@gmail.com>
2019-12-15 14:04:40 +0300

    first task v1

---
+++ script.js
@@ -0,0 +1,24 @@
+
+//первое задание
+
+let money = +prompt ('Ваш бюджет за месяц?'),
+    time = prompt ('Введите дату в формате YYYY-MM-DD'),
+    appDate = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let costs = prompt ('Введите обязательную статью расходов в этом месяце'),
+    howMany = +prompt ('Во сколько обойдётся');
+
+appDate.expenses[costs] = howMany;
+console.log(appDate);
+const totalNumberDay = 30;
+let budgetOfDay = (money - howMany) / totalNumberDay ;
+alert ('Ваш бюджет на день в ' + appDate.timeData + '  составляет: ' + budgetOfDay);
+
+console.log (budgetOfDay);
